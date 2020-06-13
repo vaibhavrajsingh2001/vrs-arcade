8f2e21a07c4e402896d875697bf143155d4f1970
Vaibhav Raj Singh <vaibhavrajsingh2001@gmail.com>
2020-06-13 14:39:07 +0530

    Completed the basic game

---
+++ readme.md
@@ -0,0 +1,33 @@
+# Shooter game with PIXI.js
+
+It's a fairly simple implementation of the PIXI.js animation library.
+
+Bundled it with webpack, so this repo can even be used as a starter code for any PIXI.js project.
+
+## Controls
+On PC 💻
+
+
+`W OR ↑ : Move ship upwards`  
+`A OR ← : Move ship left`  
+`S OR ↓ : Move ship down`  
+`D OR → : Move ship left`  
+
+`Left Click: Shoot lasers`
+
+`P : Play audio`  
+`M : Mute aduio`
+
+On phones 📱
+
+`Drag around the ship.`  
+`Click on the screen to shoot lasers`
+
+## Instructions
+
+Each time you hit the enemy:
+* Your score increases by 1.
+* The enemy speed increases by 1.
+* The speed of lasers increases by 0.5
+
+High Score is stored in local storage.
--- src/js/index.js
+++ src/js/index.js
@@ -1,84 +1,156 @@
 import * as PIXI from 'pixi.js';
 
-const animatedDiv = document.querySelector('.animated-div');
+let player, enemy;
 let keys = {};
 let bullets = [];
-const bulletSpeed = 10;
+let score = 0,
+  highScore = 0,
+  bulletSpeed = 10,
+  enemySpeed = 5;
+let verticalDirection = 'up',
+  horizontalDirection = 'right';
+const animatedDiv = document.querySelector('.animated-div');
+const audio = document.querySelector('#audio');
 
 let app = new PIXI.Application({
-	width: window.innerWidth, // default: 800
-	height: window.innerHeight, // default: 600
-	antialias: true, // default: false
-	backgroundColor: 0x061639,
-	autoResize: true,
+  width: window.innerWidth, // default: 800
+  height: window.innerHeight, // default: 600
+  antialias: true, // default: false
+  transparent: true,
+  autoResize: true,
 });
 app.renderer.view.style.display = 'block';
 animatedDiv.appendChild(app.view);
 
 // first sprite
 
-let player = new PIXI.Sprite.from('assets/player.png');
+player = new PIXI.Sprite.from('assets/player.png');
 player.anchor.set(0.5);
 player.x = app.view.width / 2;
 player.y = app.view.height / 2;
 app.stage.addChild(player);
 
-// control using mouse
-// app.stage.interactive = true;
-// const movePlayer = (e) => {
-// 	// e contains the entire event data. We'll extract it
-// 	let pos = e.data.global;
+// enemy sprite
+const spawnEnemy = () => {
+  enemy = new PIXI.Sprite.from('assets/enemy.png');
+  enemy.x = Math.random() * app.view.width;
+  enemy.y = Math.random() * app.view.height;
+  app.stage.addChild(enemy);
+};
 
-// 	player.x = pos.x;
-// 	player.y = pos.y;
-// };
-// app.stage.on('pointermove', movePlayer);
+//spawn first enemy
+spawnEnemy();
+
+const updateEnemy = () => {
+  if (verticalDirection === 'up') {
+    enemy.y -= enemySpeed;
+  }
+  if (verticalDirection === 'down') {
+    enemy.y += enemySpeed;
+  }
+  if (horizontalDirection === 'right') {
+    enemy.x += enemySpeed;
+  }
+  if (horizontalDirection === 'left') {
+    enemy.x -= enemySpeed;
+  }
+};
+
+// score card
+const style = new PIXI.TextStyle({
+  fontFamily: 'Roboto',
+  fill: ['#ffffff'],
+  fontSize: 16,
+});
+const scoreCard = new PIXI.Text('Score: ' + score, style);
+app.stage.addChild(scoreCard);
+
+// high score card
+if (localStorage.getItem('highScore')) {
+  highScore = localStorage.getItem('highScore');
+}
+const highScoreCard = new PIXI.Text('High Score: ' + highScore, style);
+highScoreCard.anchor.set(1, 0);
+highScoreCard.x = app.view.width;
+app.stage.addChild(highScoreCard);
+
+// control using mouse
+app.stage.interactive = true;
+const movePlayer = (e) => {
+  // e contains the entire event data. We'll extract it
+  let pos = e.data.global;
+
+  player.x = pos.x;
+  player.y = pos.y;
+};
+app.stage.on('pointermove', movePlayer);
 
 // control using keyboard
 const keydown = (e) => {
-	keys[e.keyCode] = true;
+  keys[e.keyCode] = true;
 };
 const keyup = (e) => {
-	keys[e.keyCode] = false;
+  keys[e.keyCode] = false;
 };
 window.addEventListener('keydown', keydown);
 window.addEventListener('keyup', keyup);
 
+const collision = (first, second) => {
+  let firstBox = first.getBounds();
+  let secondBox = second.getBounds();
+
+  return (
+    firstBox.x + firstBox.width > secondBox.x &&
+    firstBox.x < secondBox.x + secondBox.width &&
+    firstBox.y + firstBox.height > secondBox.y &&
+    firstBox.y < secondBox.y + secondBox.height
+  );
+};
+
 /* manage bullet fire system from here */
 // create a bullet
 const createBullet = () => {
-	let bullet = new PIXI.Sprite.from('assets/bullet.png');
-	bullet.anchor.set(0.5);
-	bullet.x = player.x;
-	bullet.y = player.y;
-	bullet.speed = bulletSpeed;
-	app.stage.addChild(bullet);
-	return bullet;
+  let bullet = new PIXI.Sprite.from('assets/bullet.png');
+  bullet.anchor.set(0.5);
+  bullet.x = player.x;
+  bullet.y = player.y;
+  bullet.speed = bulletSpeed;
+  app.stage.addChild(bullet);
+  return bullet;
 };
 
 // fire a bullet
 const fireBullet = () => {
-	console.log('Fired!!');
-	let bullet = createBullet();
-	bullets.push(bullet);
+  console.log('Fired!!');
+  let bullet = createBullet();
+  bullets.push(bullet);
 };
 
 // manage the array of bullets and move each bullet
 const updateBullets = (delta) => {
-	for(let i = 0; i < bullets.length; i++) {
-        bullets[i].position.y -= bullets[i].speed;
+  for (let i = 0; i < bullets.length; i++) {
+    bullets[i].position.y -= bullets[i].speed;
 
-        if(bullets[i].position.y < 0){
-            bullets[i].dead = true;
-        }
+    if (bullets[i].position.y < 0) {
+      bullets[i].dead = true;
     }
+  }
 
-    for (let i = 0; i < bullets.length; i++) {
-		if (bullets[i].dead) {
-            app.stage.removeChild(bullets[i]);
-            bullets.splice(i,1);
-		}
+  // check if bullet hits the enemy and remove it if so
+  for (let i = 0; i < bullets.length; i++) {
+    if (collision(bullets[i], enemy)) {
+      app.stage.removeChild(enemy);
+      enemySpeed += 1;
+      bulletSpeed += 0.5;
+      score += 1;
+      scoreCard.text = 'Score: ' + score;
+      spawnEnemy();
     }
+    if (bullets[i].dead) {
+      app.stage.removeChild(bullets[i]);
+      bullets.splice(i, 1);
+    }
+  }
 };
 
 // add an eventlistener for mouse click
@@ -86,20 +158,46 @@ animatedDiv.addEventListener('pointerdown', fireBullet);
 
 // create a function to will be attached to the event loop in background
 const gameLoop = (delta) => {
-	if (keys['37'] || keys['65']) {
-		player.x -= 10;
-	}
-	if (keys['38'] || keys['87']) {
-		player.y -= 10;
-	}
-	if (keys['39'] || keys['68']) {
-		player.x += 10;
-	}
-	if (keys['40'] || keys['83']) {
-		player.y += 10;
-	}
+  if (keys['37'] || keys['65']) {
+    player.x -= 10;
+  }
+  if (keys['38'] || keys['87']) {
+    player.y -= 10;
+  }
+  if (keys['39'] || keys['68']) {
+    player.x += 10;
+  }
+  if (keys['40'] || keys['83']) {
+    player.y += 10;
+  }
+  if(keys['77']) {
+	  audio.pause();
+  }
+  if(keys['80']) {
+	  audio.play();
+  }
 
-	updateBullets(delta);
+  updateBullets(delta);
+  updateEnemy();
+
+  if (enemy.x > app.view.width) {
+    horizontalDirection = 'left';
+  }
+  if (enemy.x < 0) {
+    horizontalDirection = 'right';
+  }
+  if (enemy.y > app.view.height) {
+    verticalDirection = 'up';
+  }
+  if (enemy.y < 0) {
+    verticalDirection = 'down';
+  }
+
+  if (score > highScore) {
+    highScore = score;
+    localStorage.setItem('highScore', highScore);
+    highScoreCard.text = 'High Score: ' + highScore;
+  }
 };
 
 // add the gameLoop to event loop so it executes
